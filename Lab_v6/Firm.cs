eaff510037baea1209cb0422d219d0405f60372c
UserXM300821 <UserXM300821@DESKTOP-SILH86J>
2022-03-18 12:10:57 +0600

    Добавьте файлы проекта.

---
+++ Lab_v6/Firm.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_v6
+{
+ 
+        class Firm
+        {
+        public enum Status
+        {
+            подходит, не_подходит
+
+        }
+        private string name, insurance;
+        private int age, salary, rating, index, index2;
+
+        private Status status;
+        public string Name
+        {
+            get { return name; }
+            set { if (name != "") name = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                age = value;
+                if (age >= 5)
+                {
+                    index++;
+
+                }
+                Console.WriteLine($"{index}");
+            }
+        }
+        public int Salary
+        {
+            get { return salary; }
+            set
+            {
+                salary = value;
+                if (salary > 70000)
+                {
+                    index++;
+                }
+            }
+        }
+        public int Rating
+        {
+            get { return rating; }
+            set
+            {
+                rating = value;
+                if (rating >= 3.5)
+                {
+
+                    index++;
+                }
+
+            }
+        }
+        public string Insurance
+        {
+            get { return insurance; }
+            set
+            {
+                insurance = value;
+                if (insurance == "да")
+                {
+
+                    index++;
+                }
+            }
+        }
+        public int Index
+        {
+            get { return index; }
+        }
+        public int Index2
+        {
+            get
+            {
+                return index2;
+
+            }
+            set
+            {
+                index2 = value;
+                if (index2 > 2) status = Status.подходит;
+                else status = Status.не_подходит;
+            }
+
+        }
+        public Status GetStatus
+        {
+            get { return status; }
+        }
+    }
+}
+
---
+++ Lab_v6/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab_v6
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Firm firm = new Firm();
+            
+            firm.Name = BoxName.Text;
+            firm.Age = Convert.ToInt32(BoxAge.Text);
+            firm.Salary= Convert.ToInt32(BoxSallary.Text);
+            firm.Rating = Convert.ToInt32(BoxRating.Text);
+            firm.Insurance = BoxInsurance.Text;
+            firm.Index2 = firm.Index;
+            boxResult.Text = ($"Фирма {firm.GetStatus}");
+        }
+    }
+}
